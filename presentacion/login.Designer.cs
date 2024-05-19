0079f1e24a49c81073de045ac8106ce6afcc40d1
EstebanRodri22 <estebandrodriguez@unicesar.edu.co>
2024-05-19 10:37:29 -0500

    personalizacion del login

--- presentacion/login.Designer.cs
+++ presentacion/login.Designer.cs
@@ -28,88 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnEntrar = new Button();
-            btnCancelar = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1 = new Panel();
             txtUsuario = new TextBox();
             txtContraseña = new TextBox();
             label1 = new Label();
-            label2 = new Label();
+            btnAcceder = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
-            // btnEntrar
+            // pictureBox1
             // 
-            btnEntrar.Location = new Point(234, 204);
-            btnEntrar.Name = "btnEntrar";
-            btnEntrar.Size = new Size(78, 39);
-            btnEntrar.TabIndex = 0;
-            btnEntrar.Text = "Entrar";
-            btnEntrar.UseVisualStyleBackColor = true;
+            pictureBox1.Image = Properties.Resources.Minimize_Icon;
+            pictureBox1.Location = new Point(561, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(15, 15);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
             // 
-            // btnCancelar
+            // pictureBox2
             // 
-            btnCancelar.Location = new Point(88, 204);
-            btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(78, 39);
-            btnCancelar.TabIndex = 1;
-            btnCancelar.Text = "Cancelar";
-            btnCancelar.UseVisualStyleBackColor = true;
+            pictureBox2.Image = Properties.Resources.Close_Icon;
+            pictureBox2.Location = new Point(582, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(15, 15);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 122, 204);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(173, 316);
+            panel1.TabIndex = 8;
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(193, 48);
+            txtUsuario.BackColor = Color.FromArgb(15, 15, 15);
+            txtUsuario.BorderStyle = BorderStyle.None;
+            txtUsuario.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsuario.ForeColor = Color.DimGray;
+            txtUsuario.Location = new Point(246, 70);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(139, 23);
-            txtUsuario.TabIndex = 2;
+            txtUsuario.Size = new Size(281, 20);
+            txtUsuario.TabIndex = 9;
+            txtUsuario.Text = "USUARIO";
             // 
             // txtContraseña
             // 
-            txtContraseña.Location = new Point(193, 115);
+            txtContraseña.BackColor = Color.FromArgb(15, 15, 15);
+            txtContraseña.BorderStyle = BorderStyle.None;
+            txtContraseña.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtContraseña.ForeColor = Color.DimGray;
+            txtContraseña.Location = new Point(246, 143);
             txtContraseña.Name = "txtContraseña";
-            txtContraseña.Size = new Size(139, 23);
-            txtContraseña.TabIndex = 3;
+            txtContraseña.Size = new Size(281, 20);
+            txtContraseña.TabIndex = 10;
+            txtContraseña.Text = "CONTRASEÑA";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(68, 48);
+            label1.Font = new Font("Century Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.DimGray;
+            label1.Location = new Point(378, 0);
             label1.Name = "label1";
-            label1.Size = new Size(46, 15);
-            label1.TabIndex = 4;
-            label1.Text = "usuario";
+            label1.Size = new Size(90, 30);
+            label1.TabIndex = 11;
+            label1.Text = "LOGIN";
             // 
-            // label2
+            // btnAcceder
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(68, 118);
-            label2.Name = "label2";
-            label2.Size = new Size(65, 15);
-            label2.TabIndex = 5;
-            label2.Text = "contraseña";
+            btnAcceder.FlatStyle = FlatStyle.Flat;
+            btnAcceder.ForeColor = Color.LightGray;
+            btnAcceder.Location = new Point(229, 225);
+            btnAcceder.Name = "btnAcceder";
+            btnAcceder.Size = new Size(311, 35);
+            btnAcceder.TabIndex = 12;
+            btnAcceder.Text = "ACCEDER";
+            btnAcceder.UseVisualStyleBackColor = true;
             // 
-            // login
+            // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(379, 281);
-            Controls.Add(label2);
+            BackColor = Color.FromArgb(15, 15, 15);
+            ClientSize = new Size(597, 316);
+            Controls.Add(btnAcceder);
             Controls.Add(label1);
             Controls.Add(txtContraseña);
             Controls.Add(txtUsuario);
-            Controls.Add(btnCancelar);
-            Controls.Add(btnEntrar);
-            Name = "login";
+            Controls.Add(panel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Login";
+            Opacity = 0.9D;
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private Button btnEntrar;
-        private Button btnCancelar;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Panel panel1;
         private TextBox txtUsuario;
         private TextBox txtContraseña;
         private Label label1;
-        private Label label2;
+        private Button btnAcceder;
     }
 }
