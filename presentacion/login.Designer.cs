55d248171c5f957239711d0fb06c0ec8e4ce92c7
EstebanRodri22 <estebandrodriguez@unicesar.edu.co>
2024-05-18 20:14:07 -0500

    creacion del login

---
+++ presentacion/login.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace presentacion
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnEntrar = new Button();
+            btnCancelar = new Button();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Location = new Point(234, 204);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(78, 39);
+            btnEntrar.TabIndex = 0;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(88, 204);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(78, 39);
+            btnCancelar.TabIndex = 1;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(193, 48);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(139, 23);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(193, 115);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(139, 23);
+            txtContraseña.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 4;
+            label1.Text = "usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(68, 118);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 15);
+            label2.TabIndex = 5;
+            label2.Text = "contraseña";
+            // 
+            // login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(379, 281);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtContraseña);
+            Controls.Add(txtUsuario);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnEntrar);
+            Name = "login";
+            Text = "login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEntrar;
+        private Button btnCancelar;
+        private TextBox txtUsuario;
+        private TextBox txtContraseña;
+        private Label label1;
+        private Label label2;
+    }
+}
