84709c1520dabe943bb8be4ef77298770adcfc1d
Diego Nieves <Danieves@unicesar.edu.co>
2024-06-11 06:57:11 -0500

    Historial de ventas

--- gui/frmVentas2.Designer.cs
+++ gui/frmVentas2.Designer.cs
@@ -38,27 +38,31 @@ namespace gui
             this.label1 = new System.Windows.Forms.Label();
             this.txtFiltro = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.barraTitulo = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.gridVentas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.barraTitulo.SuspendLayout();
             this.SuspendLayout();
             // 
             // gridVentas
             // 
+            this.gridVentas.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.gridVentas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridVentas.Location = new System.Drawing.Point(67, 44);
+            this.gridVentas.Location = new System.Drawing.Point(68, 89);
             this.gridVentas.Name = "gridVentas";
             this.gridVentas.Size = new System.Drawing.Size(417, 281);
             this.gridVentas.TabIndex = 0;
             // 
             // btnCerrar
             // 
-            this.btnCerrar.BackColor = System.Drawing.SystemColors.Control;
-            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnCerrar.IconChar = FontAwesome.Sharp.IconChar.X;
-            this.btnCerrar.IconColor = System.Drawing.SystemColors.ControlText;
+            this.btnCerrar.IconColor = System.Drawing.SystemColors.ButtonHighlight;
             this.btnCerrar.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnCerrar.IconSize = 27;
-            this.btnCerrar.Location = new System.Drawing.Point(3, 3);
+            this.btnCerrar.Location = new System.Drawing.Point(12, 19);
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.Size = new System.Drawing.Size(27, 27);
             this.btnCerrar.TabIndex = 1;
@@ -68,16 +72,16 @@ namespace gui
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(218, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 21);
+            this.label1.Size = new System.Drawing.Size(163, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "HISTORIAL VENTAS";
             // 
             // txtFiltro
             // 
-            this.txtFiltro.Location = new System.Drawing.Point(183, 346);
+            this.txtFiltro.Location = new System.Drawing.Point(188, 390);
             this.txtFiltro.Name = "txtFiltro";
             this.txtFiltro.Size = new System.Drawing.Size(205, 20);
             this.txtFiltro.TabIndex = 3;
@@ -86,7 +90,7 @@ namespace gui
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(67, 345);
+            this.button1.Location = new System.Drawing.Point(48, 390);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(64, 20);
             this.button1.TabIndex = 4;
@@ -94,21 +98,46 @@ namespace gui
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // barraTitulo
+            // 
+            this.barraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(32)))), ((int)(((byte)(40)))));
+            this.barraTitulo.Controls.Add(this.label3);
+            this.barraTitulo.Controls.Add(this.btnCerrar);
+            this.barraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.barraTitulo.Name = "barraTitulo";
+            this.barraTitulo.Size = new System.Drawing.Size(586, 63);
+            this.barraTitulo.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(161, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(233, 30);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Historial De Ventas";
+            // 
             // frmVentas2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(551, 390);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(98)))), ((int)(((byte)(163)))));
+            this.ClientSize = new System.Drawing.Size(586, 440);
+            this.Controls.Add(this.barraTitulo);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtFiltro);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnCerrar);
             this.Controls.Add(this.gridVentas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmVentas2";
             this.Text = "frmVentas";
             ((System.ComponentModel.ISupportInitialize)(this.gridVentas)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.barraTitulo.ResumeLayout(false);
+            this.barraTitulo.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +150,8 @@ namespace gui
             private Label label1;
             private TextBox txtFiltro;
             private Button button1;
-        }
+        private Panel barraTitulo;
+        private Label label3;
+    }
     
 }
